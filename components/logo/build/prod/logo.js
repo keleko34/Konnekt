450072cf6e9cf485b3f8e91387e7b802236965f5
keleko34 <gregory.guidero@gmail.com>
2017-04-19 01:52:24 +0200

    add logo css

--- components/logo/build/prod/logo.js
+++ components/logo/build/prod/logo.js
@@ -14,7 +14,7 @@ function logo()
 
 /* PROTOTYPES */
 
-logo.prototype.k_html = "<!-- logo Created by keleko34, displays main logo --><div class='logo'>  <img class='logo__img' src='./assets/img/logo/logo.png' />  <div class='logo__text'>Konnekt</div></div>";
-logo.prototype.k_css = "/********************************* *  logo *  Created by keleko34 *  displays main logo ********************************/.logo {}";
+logo.prototype.k_html = "<!-- logo Created by keleko34, displays main logo --><div class='logo'>  <div class='logo__container'><img class='logo__container__img' src='./assets/img/logo/logo.png' /></div>  <div class='logo__text'>Konnekt</div></div>";
+logo.prototype.k_css = "/********************************* *  logo *  Created by keleko34 *  displays main logo ********************************/.logo {  position: relative;}.logo__container {  position: absolute;  left: 10px;  top: 10px;}.logo__container__img {  height: 34px;}.logo__text {    position: absolute;    left: 64px;    top: 6px;    font-family: 'wolf_in_the_cityregular';    font-size: 58px;    color: #87cefa;}";
 return logo;
 }());
--- components/navbar/build/prod/navbar.js
+++ components/navbar/build/prod/navbar.js
@@ -15,6 +15,6 @@ function navbar()
 /* PROTOTYPES */
 
 navbar.prototype.k_html = "<!-- navbar Created by keleko34, the header navbar for the site --><div class='navbar'>  <logo></logo>  <navitem link='#documentation'>Documentation</navitem>  <navitem link='#documentation'>Playground</navitem></div>";
-navbar.prototype.k_css = "/********************************* *  navbar *  Created by keleko34 *  the header navbar for the site ********************************/.navbar {}";
+navbar.prototype.k_css = "/********************************* *  navbar *  Created by keleko34 *  the header navbar for the site ********************************/.navbar {  height:60px;  background:#333;}";
 return navbar;
 }());
