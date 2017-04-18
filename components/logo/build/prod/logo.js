430ec790d9dbead743e6846c128e022c67855dde
keleko34 <gregory.guidero@gmail.com>
2017-04-19 01:34:20 +0200

    add fonts

--- components/logo/build/prod/logo.js
+++ components/logo/build/prod/logo.js
@@ -14,7 +14,7 @@ function logo()
 
 /* PROTOTYPES */
 
-logo.prototype.k_html = "<!-- logo Created by keleko34, displays main logo --><div class='logo'>  <img class='logo__img' src='../../assets/img/logo/logo.png' />  <div class='logo__text'>Konnekt</div></div>";
+logo.prototype.k_html = "<!-- logo Created by keleko34, displays main logo --><div class='logo'>  <img class='logo__img' src='./assets/img/logo/logo.png' />  <div class='logo__text'>Konnekt</div></div>";
 logo.prototype.k_css = "/********************************* *  logo *  Created by keleko34 *  displays main logo ********************************/.logo {}";
 return logo;
 }());
