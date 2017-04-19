eac527889bf6e11401d60b303b33d35c4e813498
keleko34 <gregory.guidero@gmail.com>
2017-04-19 02:20:58 +0200

    place nav items

--- components/navitem/build/prod/navitem.js
+++ components/navitem/build/prod/navitem.js
@@ -23,6 +23,6 @@ function navitem()
 /* PROTOTYPES */
 
 navitem.prototype.k_html = "<!-- navitem Created by keleko34, a single clickable navitem for the navbar --><div class='navitem' onclick='{{onclick}}'>  <div class='navitem_text'>{{innerHTML}}</div></div>";
-navitem.prototype.k_css = "/********************************* *  navitem *  Created by keleko34 *  a single clickable navitem for the navbar ********************************/.{{local}} .navitem {  position: absolute;  height: 100%;  left:25%;  margin-left:{{left}}px}.{{local}} .navitem:hover {  background: #1b1a1a;  box-shadow: 0px -2px 16px -4px #000 inset;}.{{local}} .navitem:active {  background: #1b1a1a;  box-shadow: 0px -2px 16px -4px #000 inset;}.{{local}} .navitem_text {  cursor: pointer;  color: #F1F1F1;  font-size: 16px;  font-family: 'Open Sans';  text-align: center;  line-height: 60px;  padding: 0px 10px;}";
+navitem.prototype.k_css = "/********************************* *  navitem *  Created by keleko34 *  a single clickable navitem for the navbar ********************************/.{{local}} .navitem {  position: absolute;  height: 100%;  margin-left:{{left}}px}.{{local}} .navitem:hover {  background: #1b1a1a;  box-shadow: 0px -2px 16px -4px #000 inset;}.{{local}} .navitem:active {  background: #1b1a1a;  box-shadow: 0px -2px 16px -4px #000 inset;}.{{local}} .navitem_text {  cursor: pointer;  color: #F1F1F1;  font-size: 16px;  font-family: 'Open Sans';  text-align: center;  line-height: 60px;  padding: 0px 10px;}";
 return navitem;
 }());
