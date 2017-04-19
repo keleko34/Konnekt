16b2511e8037181fc9adfed5931e028aae35f79c
keleko34 <gregory.guidero@gmail.com>
2017-04-20 00:52:41 +0200

    build qa

---
+++ components/navitem/build/qa/navitem.js
@@ -0,0 +1,42 @@
+if(!K_Components) K_Components = {};
+K_Components["navitem"] = (function(){
+/*********************************
+ *  navitem
+ *  Created by keleko34
+ *  a single clickable navitem for the navbar
+ ********************************/
+
+function navitem()
+{
+  var self = this;
+
+  this.link = "";
+  this.left = 0;
+  this.title = "";
+  this.isActive = false;
+  
+  /* Filters */
+  this.filters.active = function(v)
+  {
+    return (v ? 'navitem--active' : '');
+  }
+  
+  /* listeners */
+  this.listen('navitem',function(value){
+    this.active = (this.title === value);
+  });
+  
+  /* ATTRIBUTES */
+  this.onclick = function()
+  {
+    self.alert('page',self.link);
+    self.alert('navitem',self.title);
+  }
+}
+
+/* PROTOTYPES */
+
+navitem.prototype.k_html = "<!-- navitem Created by keleko34, a single clickable navitem for the navbar --><div class='navitem {{isActive | active}}' onclick='{{onclick}}'>  <div class='navitem_text'>{{title}}</div></div>";
+navitem.prototype.k_css = "/********************************* *  navitem *  Created by keleko34 *  a single clickable navitem for the navbar ********************************/.{{local}} .navitem {  position: absolute;  height: 100%;  margin-left:{{left}}px}.{{local}} .navitem:hover {  background: #1b1a1a;  box-shadow: 0px -2px 16px -4px #000 inset;}.{{local}} .navitem:active {  background: #1b1a1a;  box-shadow: 0px -2px 16px -4px #000 inset;}.{{local}} .navitem--active {  background: #1b1a1a;  box-shadow: 0px -2px 16px -4px #000 inset;}.{{local}} .navitem_text {  cursor: pointer;  color: #F1F1F1;  font-size: 14px;  font-family: 'Open Sans';  text-align: center;  line-height: 60px;  padding: 0px 10px;}";
+return navitem;
+}());
---
+++ components/webapp/build/qa/webapp.js
@@ -0,0 +1,25 @@
+if(!K_Components) K_Components = {};
+K_Components["webapp"] = (function(){
+/*********************************
+ *  webapp
+ *  Created by keleko34
+ *  The main entry point for the konnekt web app
+ ********************************/
+
+function webapp()
+{
+  /* ATTRIBUTES */
+  this.page = "";
+  
+  this.listen('page',function(value){
+    this.page = value;
+    console.log(value);
+  });
+}
+
+/* PROTOTYPES */
+
+webapp.prototype.k_html = "<!-- webapp Created by keleko34, The main entry point for the konnekt web app --><div class='webapp'>  <navbar></navbar>  <div class='webapp__content'>  </div></div>";
+webapp.prototype.k_css = "/********************************* *  webapp *  Created by keleko34 *  The main entry point for the konnekt web app ********************************/.webapp {}";
+return webapp;
+}());
