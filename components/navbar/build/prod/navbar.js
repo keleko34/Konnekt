b13d6f130d5ee7c2888d8e59872fc525ad2dbeec
keleko34 <gregory.guidero@gmail.com>
2017-04-20 00:31:54 +0200

    update konnekt and add more navbar items

--- components/navbar/build/prod/navbar.js
+++ components/navbar/build/prod/navbar.js
@@ -14,7 +14,7 @@ function navbar()
 
 /* PROTOTYPES */
 
-navbar.prototype.k_html = "<!-- navbar Created by keleko34, the header navbar for the site --><div class='navbar'>  <logo></logo>  <navitem link='documentation' left='190'>Documentation</navitem>  <navitem link='playground' left='327'>Playground</navitem></div>";
+navbar.prototype.k_html = "<!-- navbar Created by keleko34, the header navbar for the site --><div class='navbar'>  <logo></logo>  <navitem link='documentation' left='190' innerHTML='Documentation'></navitem>  <navitem link='get_started' left='327' innerHTML='Get Started'></navitem>  <navitem link='download' left='464' innerHTML='Download'></navitem>  <navitem link='playground' left='601' innerHTML='Playground'></navitem></div>";
 navbar.prototype.k_css = "/********************************* *  navbar *  Created by keleko34 *  the header navbar for the site ********************************/.navbar {  position: relative;  height:60px;  background:#333;  border-bottom: 2px solid #000;  box-shadow: 0px 0px 8px -2px #000;}";
 return navbar;
 }());
