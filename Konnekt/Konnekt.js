f8c10bd5def26674c702acf1a7b38e449a16eea4
Keleko34 <gregory.guidero@gmail.com>
2017-07-05 16:22:47 +0200

    update konnekt-streams manager with new functionality and abilities, grouping, project init, multi component building

--- Konnekt/Konnekt.js
+++ Konnekt/Konnekt.js
@@ -831,6 +831,9 @@ define(['KonnektDT','KonnektL','kb','KonnektMP','KonnektRTF'],function(CreateDat
         }
       }
       recSet(v,_config);
+      
+      _hashrouter.base((_config.base !== undefined ? _config.base : 'default'));
+      
       return Konnekt;
     };
     
@@ -1065,7 +1068,7 @@ define(['KonnektDT','KonnektL','kb','KonnektMP','KonnektRTF'],function(CreateDat
         if(err)
         {
           checkBower_config(function(config,err){
-            if(err) console.error("The Konnekt local config was not found, this issue happens when You do not have Konnekt isntalled via the standard node_modules or bower_components module directories");
+            if(err) console.error("The Konnekt local config was not found, this issue happens when You do not have Konnekt installed via the standard node_modules or bower_components module directories");
             checkLocal_config(function(config,err){
               
               /* this is the finish of the chain */
